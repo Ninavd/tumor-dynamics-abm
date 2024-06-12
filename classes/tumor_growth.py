3eb4cf68a93395c8af5a7d0c77fa4871e76d43d6
Nina van der Meulen <ninaajwzz@gmail.com>
2024-06-12 14:54:14 +0200

    degradation function edited and small call errors fixed

--- classes/tumor_growth.py
+++ classes/tumor_growth.py
@@ -18,10 +18,10 @@ class TumorGrowth(Model):
         self.height = height
         self.width = width
 
-        self.center = (height - 1) /2 
-        self.grid = MultiGrid(self.height, self.width)
-        self.ecm_layer = PropertyLayer(self.height, self.width)
-        self.nutrient_layer = PropertyLayer(self.height, self.width)
+        self.center = int((height - 1) /2)
+        self.grid = MultiGrid(self.height, self.width, torus=False)
+        self.ecm_layer = PropertyLayer("ECM", self.height, self.width, default_value=np.float64(0.0))
+        self.nutrient_layer = PropertyLayer("Nutrients", self.height, self.width, default_value=np.float64(1.0))
 
         self.k = 0.02
         self.tau = 1
@@ -32,6 +32,8 @@ class TumorGrowth(Model):
 
         self.init_grid()
 
+        # TODO: place single proliferative cell in the center
+
     def init_grid(self):
         '''
         This method initializes the ECM and nutrient field
@@ -40,37 +42,47 @@ class TumorGrowth(Model):
             for y in range(self.height):
                 value = random.uniform(0,1)
                 self.ecm_layer.set_cell((x,y), value)
-                if x == 0 or x == self.width or y == 0 or y == self.height:
+                if x == 0 or x == self.width - 1 or y == 0 or y == self.height - 1:
                     self.nutrient_layer.set_cell((x,y), value=1)
                 else:
                     self.nutrient_layer.set_cell((x,y), value=0)
 
-        while self.nutrient_layer[self.center, self.center] == 0:
-            self.diffusion(self)
+        while self.nutrient_layer.data[self.center, self.center] == 0:
+            self.diffusion()
 
     def degredation(self):
-        # TODO: loop over cells with active agents (non-necrotic) instead of all cells
-        for x in range(self.width):
-            for y in range(self.height):
-                if self.ecm_layer[x,y] != 0:
-                    neighbors = self.model.grid.get_neighborhood(self.pos, moore = True, include_center = True)
-                    amount_of_tumor_cells = len(self.model.grid.get_cell_list_contents([neighbors]))
+        """
+        Attack the ECM.
+        """
+        # select cells with non-zero ECM
+        active_cells = self.ecm_layer.select_cells(lambda data: data != 0)
+        
+        # degrade ECM
+        for x, y in active_cells:
+            neighbors = self.grid.get_neighborhood(pos=(x, y), moore = True, include_center = True)
+            amount_of_tumor_cells = len(self.grid.get_cell_list_contents(neighbors))
+            updated_value = self.ecm_layer.data[x,y] - self.gamma * amount_of_tumor_cells
 
-                    self.ecm_layer.set_cell((x,y), (self.ecm_layer - self.gamma * amount_of_tumor_cells))
-                else:
-                    pass
+            # Updated ECM cannot be negative
+            updated_value = updated_value if updated_value > 0 else 0 
+            self.ecm_layer.set_cell((x,y), updated_value)
 
-    def diffusion(self, N_t):
-
+    def diffusion(self):
         for j in range(1, self.grid.width-1):
             for l in range(1, self.grid.height-1):
-                N_t = len(self.model.grid.get_cell_list_contents([j,l]))
+                N_t = len(self.grid.get_cell_list_contents([j,l]))
                 value = self.diffusion_equation(N_t, j, l)
                 self.nutrient_layer.set_cell((j,l), value)
 
     def diffusion_equation(self, N_t, x, y):
         # This equation breaks if you don't update after each grid visited and if you dont move from x,y = 0,0 to x,y max (when quation about this ask thomas or kattelijn)
-        u_next_step = ((1 + self.k * N_t * self.tau - 2 * self.lam)/ 1 + 2* self.lam) * self.nutrient_layer[x, y] + self.lam / (1 + 2 * self.lam) * (self.nutrient_layer[x + 1, y] + self.nutrient_layer[x, y + 1] + self.nutrient_layer[x - 1, y] + self.nutrient_layer[x, y - 1])
+        u_next_step = (
+            ((1 + self.k * N_t * self.tau - 2 * self.lam) / 1 + 2* self.lam) \
+            * self.nutrient_layer.data[x, y] \
+            + self.lam / (1 + 2 * self.lam) * 
+            (self.nutrient_layer.data[x + 1, y] + self.nutrient_layer.data[x, y + 1] \
+            + self.nutrient_layer.data[x - 1, y] + self.nutrient_layer.data[x, y - 1])
+        )
         
         return u_next_step
 
--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from classes.tumor_growth import TumorGrowth
 from classes.tumor_cell import TumorCell
+import matplotlib.pyplot as plt
 
 def main():
-    pass
+    model = TumorGrowth()
+    model.degredation()
+
+    plt.imshow(model.ecm_layer.data)
+    plt.title('ECM field')
+    plt.colorbar()
+    plt.show()
+
+    for i in range(100):
+        model.diffusion()
+
+    plt.imshow(model.nutrient_layer.data)
+    plt.title('Nutrient field')
+    plt.colorbar()
+    plt.show()
+
+main()
