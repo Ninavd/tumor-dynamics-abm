ac92bb51b8d945e1b296b48d29c13426d959a6df
ThomasGlansdorp <thomas.glansdorp@gmail.com>
2024-06-12 10:47:52 +0200

    Co-authored-by: Kattelijn <Kattelijn@users.noreply.github.com> Co-authored-by: Victor Piaskowski <BiggusVickus@users.noreply.github.com>

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
 import random
 
 from mesa import Model, Agent
-from mesa.space import MultiGrid
+from mesa.space import MultiGrid, PropertyLayer
 from mesa.datacollection import DataCollector
 from mesa.time import RandomActivation
 import numpy as np
+from math import e
 
 class TumorCell(Agent):
     '''
@@ -15,6 +16,13 @@ class TumorCell(Agent):
         self.state = 'proliferating'
         self.age = 0
         self.health = 1
+        self.app = 0.1
+        self.api = 0.1
+        self.bii = 0.1
+        self.bip = 0.1
+        self.ECM = 1
+        self.theta_i = 1
+        self.theta_p = 1
 
     def step(self):
         '''
@@ -25,7 +33,10 @@ class TumorCell(Agent):
         # get neighbouring tumor cells states
         # get neighbors with 0 ECM
         neighbors = self.model.grid.get_neighborhood(self.pos, moore = True, include_center = False)
-        
+        for neighbor in neighbors:
+            if self.model.grid.get_cell_list_contents(neighbor) == 0:
+                open_cell = neighbor
+                break
         
         
         if self.state == 'proliferating':
@@ -35,6 +46,56 @@ class TumorCell(Agent):
             self.migrate(open_cell)
         elif self.state == 'necrotic':
             self.die()
+        pass
+
+    def probability_proliferate(self):
+        '''
+        This method should return the probability of the tumor cell to proliferate.
+        '''
+        N_T = 0
+        cell_contents = self.model.grid.get_cell_list_contents(self.pos)
+        cell_contents.remove(self)
+        N_T = len(cell_contents)
+        left = (1 - e^(-(self.ECM/(N_T * self.theta_p))**2)) 
+
+        
+        right = 1
+        for i in range(len(cell_contents)):
+            if cell_contents[i].state == 'proliferating':
+                right *= 1 + self.app
+            if cell_contents[i].state == 'invasion':
+                right *= 1 + self.api
+        return left * right
+
+    def probability_invasion(self):
+        '''
+        This method should return the probability of the tumor cell to invade.
+        '''
+        N_T = 0
+        cell_contents = self.model.grid.get_cell_list_contents(self.pos)
+        cell_contents.remove(self)
+        N_T = len(cell_contents)
+        left = (1 - e^(-(self.ECM/(N_T * self.theta_p))**2)) 
+
+        
+        right = 1
+        for i in range(len(cell_contents)):
+            if (cell_contents[i].state == 'proliferating'):
+                right *= 1 + self.bip
+            if (cell_contents[i].state == 'invasion'):
+                right *= 1 + self.bii
+        return left * right
+
+    def probability_migrate(self):
+        '''
+        This method should return the probability of the tumor cell to migrate.
+        '''
+        pass
+
+    def probability_necrotic(self):
+        '''
+        This method should return the probability of the tumor cell to become necrotic.
+        '''
         pass
     
     def migrate(self, open_cell):
@@ -74,7 +135,19 @@ class TumorGrowth(Model):
 
 
         self.grid = MultiGrid(self.height, self.width)
+        self.ecm_layer = PropertyLayer(self.height, self.width)
 
+    def init_grid(self):
+        '''
+        This method 
+        '''
+        Proper
+
+    def step(self):
+        # degradation ecm
+        # diffusion step
+        # determine cell death
+        # tumor cell steps
         
 
 
