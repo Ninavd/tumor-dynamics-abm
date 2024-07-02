5d2c80868bdb8161e4282c84bb34030aab295874
Nina van der Meulen <ninaajwzz@gmail.com>
2024-07-02 22:17:15 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,62 +1,55 @@
 import argparse
 import numpy as np
-from classes.tumor_growth import TumorGrowth
-from classes.tumor_cell import TumorCell
-from classes.tumor_visualizations import TumorVisualization
-import matplotlib.pyplot as plt
 import warnings
-import pickle
-import time
-from helpers import save_timestamp_metadata, build_and_save_animation
+
+from classes.tumor_growth import TumorGrowth
+from classes.tumor_visualizations import TumorVisualization
+from helpers import save_timestamp_metadata, build_and_save_animation, print_summary_message
+
+# surpress warning about new PropertyLayer class of mesa
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+
 def main(steps, dd, L, seed, payoff, voronoi, summary, save, show_plot, animate):
-    
+    """
+    Run agent-based tumor growth model with provided command-line arguments.
+    """
+
+    # initialize model
     model = TumorGrowth(steps=steps, delta_d=dd, app=payoff[0][0], api=payoff[0][1], bip=payoff[1][0], bii=payoff[1][1], width=L, height=L, seed=seed, distribution='voronoi' if voronoi else 'uniform')
 
-    _, _, _, _, steps = model.run_model()
-   
+    # run a simulation
+    radius, _, roughness, velocity, steps_taken = model.run_model()
+    
     if summary:
-        print(
-            f"""
-            ****************** SUMMARY ***************************
-            * iterations \t - {len(model.N_Ts) -1}
-            * grid size \t - {L}x{L}
-            * seed \t \t - {seed}
-            * payoff matrix \t - {payoff}
-            * ECM \t\t - {'voronoi' if voronoi else 'uniform'}
-            * final #(proliferating) \t - {model.proliferating_cells[-1]}
-            * final #(invasive) \t - {model.invasive_cells[-1]}
-            * final #(necrotic) \t - {model.necrotic_cells[-1]}
-            * final roughness \t - 
-            * final tumor size \t - 
-            *******************************************************
-            """
-        )
+        print_summary_message(model, steps_taken, payoff, roughness, radius, velocity)
 
     if save:
-        # model.save_simulation_results_to_file()
-        timestamp = str(time.time()).split('.')[0]
-        with open(f'save_files/simulation_data_{timestamp}.pickle', 'wb') as f:
-            pickle.dump(model, f)
+        # save pickle of final tumor object 
+        timestamp = model.save_simulation_results_to_file()
         save_timestamp_metadata(model, timestamp)
     
     if show_plot:
         vis = TumorVisualization(model)
-        # vis.plot_all()
-        # vis.plot_cell_types()
-        # vis.plot_proportion_cell_types()
-        # vis.plot_tumor_over_time(steps)
+        vis.plot_all()
+        vis.plot_cell_types()
+        vis.plot_proportion_cell_types()
+        vis.plot_tumor_over_time(steps)
         vis.plot_radial_distance()
-        # vis.plot_roughness()
-        # vis.plot_distribution()
-        # vis.plot_velocities()
+        vis.plot_roughness()
+        vis.plot_velocities()
     
     if animate:
+        
+        # save animation of tumor to mp4
         n_frames = 100
         stepsize = int(len(model.N_Ts) / n_frames)
         frames = model.N_Ts[::stepsize]
-        build_and_save_animation(frames, title='test', iterations=n_frames)
+        
+        title = f'videos/{steps_taken}_steps_{model.distribution}_ECM_seed{seed}.mp4'
+        
+        build_and_save_animation(frames, title=title, iterations=n_frames)
+
 
 if __name__ == "__main__":
     # set-up parsing command line arguments
@@ -74,7 +67,7 @@ if __name__ == "__main__":
     parser.add_argument('--voronoi', action="store_true", help="Initialize ECM grid as voronoi diagram instead of uniform")
     parser.add_argument('--summary', action="store_true", help="print summary of simulation results")
     parser.add_argument("--save", action="store_true", help="store simulation object in pickle file")
-    parser.add_argument("--show_plot", action="store_true", help="show plot of final tumor")
+    parser.add_argument("--show_plot", action="store_true", help="show plot of final tumor and other parameters")
     parser.add_argument("--animate", action="store_true", help="save animation video of tumor growth")
 
     # read arguments from command line
