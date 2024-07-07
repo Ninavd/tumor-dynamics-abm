d8f0d5332a2fa39fc4ff1671a7ad5f8ad980988c
Nina van der Meulen <ninaajwzz@gmail.com>
2024-07-07 02:49:27 +0200

    option for averaging added, necrotic plot added

--- main.py
+++ main.py
@@ -4,22 +4,31 @@ import warnings
 
 from classes.tumor_growth import TumorGrowth
 from classes.tumor_visualizations import TumorVisualization
+from classes.collect_averages import RunCollection
 from helpers import save_timestamp_metadata, build_and_save_animation, print_summary_message
 
 # surpress warning about new PropertyLayer class of mesa
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-def main(steps, L, seed, payoff, voronoi, summary, save, show_plot, animate):
+def main(steps, L, n_runs, seed, payoff, voronoi, summary, save, show_plot, animate):
     """
     Run agent-based tumor growth model with provided command-line arguments.
     """
 
     # initialize model
     model = TumorGrowth(steps=steps, app=payoff[0][0], api=payoff[0][1], bip=payoff[1][0], bii=payoff[1][1], width=L, height=L, seed=seed, distribution='voronoi' if voronoi else 'uniform')
-
-    # run a simulation
-    radius, _, roughness, velocity, steps_taken = model.run_model()
+    
+    # run a single simulation or multiple
+    if n_runs == 1:
+        radius, _, roughness, velocity, steps_taken = model.run_model()
+    else:
+        runs = RunCollection(n_runs, steps=steps, app=payoff[0][0], api=payoff[0][1], bip=payoff[1][0], bii=payoff[1][1], width=L, height=L, distribution='voronoi' if voronoi else 'uniform')
+        model, results = runs.run()
+        steps_taken = steps
+        roughness   = results['roughness'][-1]
+        radius      = results['radius'][-1]
+        velocity    = results['velocity'][-1]
     
     if summary:
         print_summary_message(model, steps_taken, payoff, roughness, radius, velocity)
@@ -31,13 +40,14 @@ def main(steps, L, seed, payoff, voronoi, summary, save, show_plot, animate):
     
     if show_plot:
         vis = TumorVisualization(model)
+        vis.plot_necrotic_cells()
         vis.plot_all()
         vis.plot_cell_types()
         vis.plot_proportion_cell_types()
         vis.plot_tumor_over_time(steps)
         vis.plot_radial_distance()
         vis.plot_roughness()
-        vis.plot_velocities()
+        vis.plot_velocities() if steps_taken > 200 else None
     
     if animate:
         
@@ -58,6 +68,7 @@ if __name__ == "__main__":
     # adding arguments
     parser.add_argument("n_steps", help="max number of time steps used in simulation", default=1000, type=int)
     parser.add_argument("L_grid", help="Width of grid in number of cells", default=201, type=int)
+    parser.add_argument("-n", help="How many runs to run. If more than one, averaged results are saved to csv. Default is 1.", default=1, type=int)
     parser.add_argument("-s", "--seed", help="provide seed of simulation", default=np.random.randint(1000), type=int)
     parser.add_argument("-api", "--alpha_pi", help="proliferative probability change when encountering an invasive cell", default=-0.02, type=float)
     parser.add_argument("-app", "--alpha_pp", help="proliferative probability change when encountering an proliferative cell", default=-0.1, type=float)
@@ -79,5 +90,5 @@ if __name__ == "__main__":
 
     # run main with provided arguments
     main(
-        args.n_steps, args.L_grid, args.seed, payoff, args.voronoi, args.summary, args.save, args.show_plot, args.animate
+        args.n_steps, args.L_grid, args.n, args.seed, payoff, args.voronoi, args.summary, args.save, args.show_plot, args.animate
         ) 
