ca62302d2efc7ede204c495e4a6e3c4ed34902c0
Nina van der Meulen <ninaajwzz@gmail.com>
2024-07-07 22:23:03 +0200

    comments added

--- tumor/sensitivity_analysis_csv_only.py
+++ tumor/sensitivity_analysis_csv_only.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 import numpy as np
 
+# dictionary to format ugly parameter names to latex
 to_latex = {
     'app':'$\\alpha_{pp}$',
     'bii':'$\\beta_{ii}$',
@@ -15,22 +16,24 @@ to_latex = {
     'k': 'k'
 }
 
+# results of first order analysis
 S1_result_files = [
-    'save_files/SA_analysis_128_distinct_samples_uniform/S1_tumor_diameter_steps_1000_grid_101_params_varied_10.csv',
-    'save_files/SA_analysis_128_distinct_samples_uniform_no_gamma/S1_tumor_diameter_steps_1000_grid_101_params_varied_9.csv',
-    'save_files/SA_analysis_128_distinct_samples_voronoi/S1_tumor_diameter_steps_1000_grid_101_params_varied_10.csv',
-    'save_files/SA_analysis_128_distinct_samples_voronoi_no_gamma/S1_tumor_diameter_steps_1000_grid_101_params_varied_9.csv',
-    'save_files/SA_analysis_1024_distinct_samples_uniform/S1_tumor_diameter_steps_1000_grid_101_params_varied_6.csv',
-    'save_files/SA_analysis_1024_distinct_samples_voronoi/S1_tumor_diameter_steps_1000_grid_101_params_varied_6.csv',
+    '../save_files/SA_analysis_128_distinct_samples_uniform/S1_tumor_diameter_steps_1000_grid_101_params_varied_10.csv',
+    '../save_files/SA_analysis_128_distinct_samples_uniform_no_gamma/S1_tumor_diameter_steps_1000_grid_101_params_varied_9.csv',
+    '../save_files/SA_analysis_128_distinct_samples_voronoi/S1_tumor_diameter_steps_1000_grid_101_params_varied_10.csv',
+    '../save_files/SA_analysis_128_distinct_samples_voronoi_no_gamma/S1_tumor_diameter_steps_1000_grid_101_params_varied_9.csv',
+    '../save_files/SA_analysis_1024_distinct_samples_uniform/S1_tumor_diameter_steps_1000_grid_101_params_varied_6.csv',
+    '../save_files/SA_analysis_1024_distinct_samples_voronoi/S1_tumor_diameter_steps_1000_grid_101_params_varied_6.csv',
     ]
 
+# results of second order analysis
 ST_result_files = [
-    'save_files/SA_analysis_128_distinct_samples_uniform/ST_tumor_diameter_steps_1000_grid_101_params_varied_10.csv',
-    'save_files/SA_analysis_128_distinct_samples_uniform_no_gamma/ST_tumor_diameter_steps_1000_grid_101_params_varied_9.csv',
-    'save_files/SA_analysis_128_distinct_samples_voronoi/ST_tumor_diameter_steps_1000_grid_101_params_varied_10.csv',
-    'save_files/SA_analysis_128_distinct_samples_voronoi_no_gamma/ST_tumor_diameter_steps_1000_grid_101_params_varied_9.csv',
-    'save_files/SA_analysis_1024_distinct_samples_uniform/ST_tumor_diameter_steps_1000_grid_101_params_varied_6.csv',
-    'save_files/SA_analysis_1024_distinct_samples_voronoi/ST_tumor_diameter_steps_1000_grid_101_params_varied_6.csv'
+    '../save_files/SA_analysis_128_distinct_samples_uniform/ST_tumor_diameter_steps_1000_grid_101_params_varied_10.csv',
+    '../save_files/SA_analysis_128_distinct_samples_uniform_no_gamma/ST_tumor_diameter_steps_1000_grid_101_params_varied_9.csv',
+    '../save_files/SA_analysis_128_distinct_samples_voronoi/ST_tumor_diameter_steps_1000_grid_101_params_varied_10.csv',
+    '../save_files/SA_analysis_128_distinct_samples_voronoi_no_gamma/ST_tumor_diameter_steps_1000_grid_101_params_varied_9.csv',
+    '../save_files/SA_analysis_1024_distinct_samples_uniform/ST_tumor_diameter_steps_1000_grid_101_params_varied_6.csv',
+    '../save_files/SA_analysis_1024_distinct_samples_voronoi/ST_tumor_diameter_steps_1000_grid_101_params_varied_6.csv'
 ]
 
 def plot_layout():
@@ -40,49 +43,52 @@ def plot_layout():
     plt.grid(axis='y', alpha=0.5)
     plt.xlim(-0.1, 1.15)
 
-for S1, ST in zip(S1_result_files, ST_result_files):
+if __name__=="__main__":
 
-    plt.figure(figsize=(8, 7))
+    # create pretty plots for all results and save
+    for S1, ST in zip(S1_result_files, ST_result_files):
 
-    print(S1)
-    print(ST)
-    df = pd.read_csv(S1)
+        plt.figure(figsize=(8, 7))
 
-    plt.subplot(121)
-    plt.errorbar(x=df['S1'], y=[to_latex[param] for param in df['Unnamed: 0']], xerr=df['S1_conf'], barsabove=True, fmt='o', markersize=6, capsize=8, color='black')
-    plt.vlines(x = 0, color='black', linestyle='dotted', alpha=0.7, ymin=-1, ymax=0.5+len(df['S1']))
-    plt.ylim(-0.5, len(df['S1']))
-    plt.xlabel('$S_1$', fontsize=18)
-    yticks, _ = plt.yticks(fontsize=18)
-    plot_layout()
+        print(S1)
+        print(ST)
+        df = pd.read_csv(S1)
 
-    df = pd.read_csv(ST)
+        plt.subplot(121)
+        plt.errorbar(x=df['S1'], y=[to_latex[param] for param in df['Unnamed: 0']], xerr=df['S1_conf'], barsabove=True, fmt='o', markersize=6, capsize=8, color='black')
+        plt.vlines(x = 0, color='black', linestyle='dotted', alpha=0.7, ymin=-1, ymax=0.5+len(df['S1']))
+        plt.ylim(-0.5, len(df['S1']))
+        plt.xlabel('$S_1$', fontsize=18)
+        yticks, _ = plt.yticks(fontsize=18)
+        plot_layout()
 
-    plt.subplot(122)
-    plt.errorbar(
-        x=df['ST'], 
-        y=[to_latex[param] for param in df['Unnamed: 0']], 
-        xerr=df['ST_conf'], 
-        barsabove=True, 
-        fmt='o', markersize=6, capsize=8, color='black'
-    )
-    plt.vlines(
-        x = 0, 
-        color='black', 
-        linestyle='dotted', 
-        alpha=0.7, 
-        ymin=-1, 
-        ymax=0.5+len(df['ST'])
-    )
-    
-    plt.yticks(ticks=yticks, labels=[])
-    plt.ylim(-0.5, len(df['ST']))
-    plt.xlabel('$S_T$', fontsize=18)
-    plot_layout()
+        df = pd.read_csv(ST)
 
-    plt.tight_layout()
+        plt.subplot(122)
+        plt.errorbar(
+            x=df['ST'], 
+            y=[to_latex[param] for param in df['Unnamed: 0']], 
+            xerr=df['ST_conf'], 
+            barsabove=True, 
+            fmt='o', markersize=6, capsize=8, color='black'
+        )
+        plt.vlines(
+            x = 0, 
+            color='black', 
+            linestyle='dotted', 
+            alpha=0.7, 
+            ymin=-1, 
+            ymax=0.5+len(df['ST'])
+        )
+        
+        plt.yticks(ticks=yticks, labels=[])
+        plt.ylim(-0.5, len(df['ST']))
+        plt.xlabel('$S_T$', fontsize=18)
+        plot_layout()
 
-    title = S1.split('/')[1][12:] + 'ST_' + S1.split('/')[-1][:-4]
-    plt.savefig(title, dpi=300)
+        plt.tight_layout()
 
-    plt.show()
+        title = S1.split('/')[1][12:] + 'ST_' + S1.split('/')[-1][:-4]
+        plt.savefig(title, dpi=300)
+
+        plt.show()
